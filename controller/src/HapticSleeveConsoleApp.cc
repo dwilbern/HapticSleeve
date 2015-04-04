33d80af90884b3e7e8b5a291b9141fc5790362a5
Daniel Wilbern <dwilbern@nmu.edu>
2015-04-04 03:12:45 -0400

    Added basic functionally to CLI interface.

--- controller/src/HapticSleeveConsoleApp.cc
+++ controller/src/HapticSleeveConsoleApp.cc
@@ -1,13 +1,40 @@
 
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+#include "HapticSleeveConsoleApp.hh"
 #include "HapticSleeveInterface.hh"
 
 int main(int argc, char *argv[]) {
 
 	HapticSleeveInterface *sleeve = new HapticSleeveInterface();
+	std::string s;
 
-	
+	while(true) {
+		std::cout << "? ";
+		getline(std::cin,s);
+
+		if(s == "") continue;
+		else if (s == "exit" || s == "quit") break;
+		else if(s == "help") PrintHelpDialog();
+		else {
+			std::cout << "Invalid command " << s << '.' << std::endl;	
+			PrintHelpDialog();
+		}
+	}
 
 	delete sleeve;
 	return 0;
+}
+
+void PrintHelpDialog() {
+
+	printf("Haptic Sleeve CLI Controller App by Daniel Wilbern, dwilbern@nmu.edu\n");	
+	printf("Commands:\n");
+	printf("\thelp: print this dialog\n");
+	printf("\tconnect <port name>: open a connection to the haptic sleeve at the given serial port location (e.g. \"COM3\")\n");
+	printf("\tdisconnect: close the serial connection to the haptic sleeve\n");
+	printf("\texit: close this program\n");
 }
 
---
+++ controller/src/HapticSleeveConsoleApp.hh
@@ -0,0 +1,9 @@
+
+#ifndef __Haptic_Sleeve_Console_App_H_
+#define __Haptic_Sleeve_Console_App_H_
+
+int main(int argc, char *argv[]);
+void PrintHelpDialog();
+
+#endif
+
