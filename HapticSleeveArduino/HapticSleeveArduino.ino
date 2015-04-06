68381c01e6c61dc64414b35c17c66357d25efd48
Daniel Wilbern <dwilbern@nmu.edu>
2015-04-05 22:51:01 -0400

    Updated Arduino program to accomodate changes.

--- HapticSleeveArduino/HapticSleeveArduino.ino
+++ HapticSleeveArduino/HapticSleeveArduino.ino
@@ -4,6 +4,7 @@
 
 #define SETPOS 'p'
 #define GETPOS 'g'
+#define CALIBRATE 'c'
 #define ECHO 'e'
 
 Servo myservo;
@@ -19,58 +20,65 @@ int degreesTolerance = 10;
 
 void setup() {
 
-	myservo.attach(servoPin);
-	Serial.begin(9600); // 9600 baud
-	myservo.write(0);
+  myservo.attach(servoPin);
+  Serial.begin(115200); // 115200 baud
+  myservo.write(0);
 }
 
 void loop() {
 
-	handleSerialCommunication();
+  handleSerialCommunication();
 }
 
 void handleSerialCommunication() {
 
-	unsigned char incomingByte;
+  unsigned char incomingByte;
 
-	if(Serial.available() > 0) {
+  if(Serial.available() > 0) {
 
-		incomingByte = Serial.read();
+    incomingByte = Serial.read();
 
-		switch(incomingByte) {
+    switch(incomingByte) {
 
-			case SETPOS:
-				targetPos = Serial.parseInt();
-				break;
+    case SETPOS:
+      targetPos = Serial.parseInt();
+      break;
 
-			case GETPOS:
-				Serial.println(getServoPos());
-				break;
+    case GETPOS:
+      Serial.println(getServoPos());
+      break;
 
-			case ECHO:
-                                delay(10);
-				while(Serial.available() > 0)
-					Serial.write(Serial.read());
-                                Serial.write('\n');
-				break;
-		}
-	}
+    case CALIBRATE:
+      calibrateServoFeedback();
+      Serial.println("done");
+      break;
+
+    case ECHO:
+    delay(10);
+      Serial.read(); // eat the space
+      while(Serial.available() > 0)
+        Serial.write(Serial.read());
+      break;
+    }
+  }
 }
 
 void calibrateServoFeedback() {
-	myservo.write(minDegrees);
-	delay(2000); // give the servo more than enough time to get there and settle
-	minFeedback = analogRead(feedbackPin);
+  myservo.write(minDegrees);
+  delay(2000); // give the servo more than enough time to get there and settle
+  minFeedback = analogRead(feedbackPin);
 
-	myservo.write(maxDegrees);
-	delay(2000); // give the servo more than enough time to get there and settle
-	maxFeedback = analogRead(feedbackPin);
+  myservo.write(maxDegrees);
+  delay(2000); // give the servo more than enough time to get there and settle
+  maxFeedback = analogRead(feedbackPin);
 }
 
 int getServoPos() {
 
-	return map(analogRead(feedbackPin), minFeedback, maxFeedback, minDegrees, maxDegrees);
+  return map(analogRead(feedbackPin), minFeedback, maxFeedback, minDegrees, maxDegrees);
 }
+
+
 
 
 
